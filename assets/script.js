e3ae2fd6aabe9ba9f6fca8eb6f4402956126d489
Josh <joshschulze05@gmail.com>
2023-12-18 21:14:33 -0500

    made changes to js

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,5 @@
 
 var timeDisplayEl = $('#time-display');
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  
-});
 
 // handle displaying the time
 function displayTime() {
@@ -51,7 +36,34 @@ if (Hours<rightNow){
 }
 
  
-UpdateHours();
 
+ function SaveData(){
+
+  $(".saveBtn").on("click", function () {
+        
+    var descriptions = $(this).siblings(".description").val();
+    var id= $(this).parent().attr("id");
+
+    
+    localStorage.setItem(id, descriptions);
+})
+
+ }
+  
+    
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+ 
+UpdateHours();
+SaveData();
 displayTime();
 setInterval(displayTime, 1000);
+
+
